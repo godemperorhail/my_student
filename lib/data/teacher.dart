86dd1cc375c208037cb7e31dd532bd9b97630fb9
Lê Thanh Nguyên <124133246+godemperorhail@users.noreply.github.com>
2023-02-01 20:21:32 +0700

    update tren git web

--- lib/data/teacher.dart
+++ lib/data/teacher.dart
@@ -3,7 +3,7 @@ import 'package:mystudent/model/teacher.dart';
 class DataTeacher {
   final teacher1 = Teacher("1",
       name: "Violet",
-      birthday: DateTime(1956, 12, 10),
+      birthday: DateTime(1956, 12, 19),
       address: "Japanese",
       phoneNumber: 0325698741,
       classroom: "Zio1",
